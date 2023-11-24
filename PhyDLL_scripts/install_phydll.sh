8bf556607376e47cd27dd708dad5cf03e368a3db
Eray Inanc <einanc@outlook.de>
2023-11-24 17:42:09 +0100

    Update install_phydll.sh

--- PhyDLL_scripts/install_phydll.sh
+++ PhyDLL_scripts/install_phydll.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 # PhyDLL installation to JURECA-DC
-# PhyDLL source can be found at https://gitlab.com/cerfacs/phydl
-
-# get phydll 0.2.0
-git clone https://gitlab.com/cerfacs/phydll -b release/0.2
-pushd phydll
-
-# create installation folder
-mkdir -p ../PHYDLL
-export BUILD=$(realpath ../PHYDLL)
 
 # load modules (here is for JURECA-DC)
 ml GCC OpenMPI Python CMake mpi4py MPI-settings/CUDA
@@ -18,6 +9,14 @@ export CC=mpicc
 # create python env
 python3 -m venv envPL
 source $PWD/envPL/bin/activate
+
+# get phydll 0.2.0
+git clone https://gitlab.com/cerfacs/phydll -b release/0.2
+pushd phydll
+
+# create installation folder
+mkdir -p ../PHYDLL
+export BUILD=$(realpath ../PHYDLL)
 
 # install PhyDLL
 make CC=$CC BUILD=../PHYDLL ENABLE_PYTHON=ON 
@@ -31,7 +30,5 @@ cp -r ./test/t0_nc ../tutorial
 # link missing lib to the envPL
 popd 
 ln -s $PWD/PHYDLL/src/python/pyphydll $PWD/envPL/lib/python3.11/site-packages
-
-pushd tutorial
 
 # eof
